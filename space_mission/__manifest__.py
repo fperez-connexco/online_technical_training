33d655e78fedf2e337ee3147db6a6661345516fd
Félix Pérez <fperez@connexco.com.do>
2022-11-09 15:34:15 +0000

    [ADD] Basic Module

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+
+{
+    'name': 'Space Mission',
+    
+    'summary': """Space Mission app to manage travel project""",
+    
+    'description': """
+        Space mission app to manage travel project:
+        - Initiation
+        - Planning
+        - Execution
+        - Monitoring and Controlling
+        - Project Closing
+    """,
+    
+    'author': 'Félix Antonio Pérez Abreu',
+    
+    'website': 'connexco.com.do',
+    
+    'category': 'Project',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    'demo': [
+        
+        
+    ],
+    
+}
